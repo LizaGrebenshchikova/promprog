089547a23113633b1ed2ecf04d4ab42d03c7eeda
LizaGrebenshchikova <grebenshchikova.liza@yahoo.com>
2018-02-26 14:49:26 +0300

    edit

--- MyToDoList/start_server.bat
+++ MyToDoList/start_server.bat
@@ -3,4 +3,4 @@ docker-machine start dockervm
 @FOR /f "tokens=*" %i IN ('docker-machine env') DO @%i
 docker-compose build                
 docker-compose up
-REM connect with dockervm IP-address
+REM connect with dockervm IP-address:8000/admin/core
